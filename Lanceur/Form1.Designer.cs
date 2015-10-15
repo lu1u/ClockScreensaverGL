08bd9aa6fdf1c32c8c3240b9dca6c9089b663aab
lu1u <github.10.pilloni@spamgourmet.com>
2015-10-15 11:41:27 +0200

    Programme intermediaire pour lancer l'économiseur et eviter que le driver ne desactive l'acceleration materielle

---
+++ Lanceur/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Lanceur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonParcourir = new System.Windows.Forms.Button();
+            this.checkBoxAttendre = new System.Windows.Forms.CheckBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ecran à lancer:";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFileName.Location = new System.Drawing.Point(99, 13);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(363, 20);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // buttonParcourir
+            // 
+            this.buttonParcourir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonParcourir.Location = new System.Drawing.Point(469, 13);
+            this.buttonParcourir.Name = "buttonParcourir";
+            this.buttonParcourir.Size = new System.Drawing.Size(75, 23);
+            this.buttonParcourir.TabIndex = 2;
+            this.buttonParcourir.Text = "Parcourir...";
+            this.buttonParcourir.UseVisualStyleBackColor = true;
+            this.buttonParcourir.Click += new System.EventHandler(this.buttonParcourir_Click);
+            // 
+            // checkBoxAttendre
+            // 
+            this.checkBoxAttendre.AutoSize = true;
+            this.checkBoxAttendre.Location = new System.Drawing.Point(16, 44);
+            this.checkBoxAttendre.Name = "checkBoxAttendre";
+            this.checkBoxAttendre.Size = new System.Drawing.Size(325, 17);
+            this.checkBoxAttendre.TabIndex = 3;
+            this.checkBoxAttendre.Text = "Attendre la fin de l\'écran de veille avant de fermer le programme";
+            this.checkBoxAttendre.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "scr";
+            this.openFileDialog.Filter = "Ecrans de veille|*.scr|Programmes|*.exe|Tous les fichies|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 77);
+            this.Controls.Add(this.checkBoxAttendre);
+            this.Controls.Add(this.buttonParcourir);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lanceur d\'écran de veille";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonParcourir;
+        private System.Windows.Forms.CheckBox checkBoxAttendre;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Lanceur/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lanceur
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower().Trim().Substring(0, 2) == "/c") //configure
+                {
+                    Application.Run(new Form1());
+                }
+                else
+                    ShowScreensaver(args);
+                    
+            }
+            else //no arguments were passed
+            {
+                Application.Run(new Form1());
+            }
+        }
+
+        private static void ShowScreensaver(string[] args)
+        {
+            if (Settings.Default.FileName.Length == 0 || !File.Exists(Settings.Default.FileName))
+            {
+                Application.Run(new Form1());
+                return;
+            }
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = Settings.Default.FileName;
+                if (args != null)
+                {
+                    for (int i = 0; i < args.Length; i++)
+                        p.StartInfo.Arguments += args[i] + " ";
+                }
+
+                p.Start();
+
+                if (Settings.Default.Attendre)
+                    p.WaitForExit();
+            }
+            catch( Exception e)
+            {
+                MessageBox.Show(e.Source + "\nFilename:" + Settings.Default.FileName, e.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
